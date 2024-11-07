206b62752086d7a88e02ef5d6120a667e9d74bf0
Minyethway93 <minyethway93@gmail.com>
2024-11-07 19:01:12 +0800

    07 Nov

--- DAC-004_Intro_to_SQL/DAC-004_Intro_to_SQL_Leetcode_Blank.sql
+++ DAC-004_Intro_to_SQL/DAC-004_Intro_to_SQL_Leetcode_Blank.sql
@@ -27,7 +27,7 @@ VALUES
 -- Explore the table:
 
 SELECT *
-FROM myownschema.numbers;
+FROM myownschema.numbers
 
 -- Q1: Write a SQL query to find only the numbers that start a sequence of at least three consecutive numbers in the table. 
 -- Form the additional two columns to illustrate the sequence of at least three consecutive numbers. Do not include numbers that can't form three consecutive numbers.
@@ -42,6 +42,13 @@ col1 col2 col3
 
 -- Answer
 
+ALTER TABLE myownschema.numbers ADD index2 INT;
+ALTER TABLE myownschema.numbers ADD index3 INT;
+
+IF
+SELECT index, index2, index3
+FROM myownschema.numbers
+GROUP BY index, index2, index3;
 --------------------------------------------------------------------------------------------------------------------------------------------------
 
 -- Run these once!
@@ -61,8 +68,13 @@ FROM myownschema.stars;
 
 -- Q2: Form a triangle that has three rows, 3 stars at the top row and 1 star at the bottom row and rectangle with 3 rows and 2 columns using this table only.
 
--- Rectangle Answer:
+-- Rectangle Answer: 
 
+ALTER TABLE myownschema.stars ADD star2 VARCHAR(1);
+UPDATE myownschema.stars SET star2 = '*';
+
+SELECT *
+FROM myownschema.stars
 
 -- Triangle Answer: 
 
--- DAC-004_Intro_to_SQL/DAC-004_Intro_to_SQL_Revision_Blank.sql
+++ DAC-004_Intro_to_SQL/DAC-004_Intro_to_SQL_Revision_Blank.sql
@@ -12,12 +12,20 @@
 
 --Answer
 
+SELECT *
+FROM humanresources.employee
+ORDER BY jobtitle ASC;
+
 -------------------------------------------------------------------------------------------------------------------------------------------
 
 -- Q2: From the following table person.person write a query in SQL to return all rows and a subset of the columns (firstName, lastName, businessentityid) from the person table in the AdventureWorks database. 
 -- The third column heading is renamed to employee_id. Arranged the output in ascending order by lastname.
 
 --Answer
+
+SELECT firstName, lastName, businessentityid AS employee_id
+FROM person.person
+ORDER BY lastName ASC;
 
 -------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -28,6 +36,11 @@
 
 --Answer
 
+SELECT productid, productnumber, name AS productname
+FROM production.product
+WHERE sellstartdate IS NOT NULL AND productline = 'T'
+ORDER BY name ASC;
+
 -------------------------------------------------------------------------------------------------------------------------------------------
 
 -- Q4:From the following table write a query in SQL to calculate the total freight paid by each customer. Return customerid and total freight. 
@@ -36,6 +49,11 @@
 -- sales.salesorderheader
 
 --Answer
+
+SELECT customerid, SUM(freight) AS total_freight 
+FROM sales.salesorderheader
+GROUP BY customerid
+ORDER BY customerid ASC;
 
 -------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -46,6 +64,14 @@
 
 --Answer
 
+SELECT address.city, COUNT(business.businessentityid) AS no_of_employees
+FROM person.address AS address
+JOIN person.businessentityaddress AS business 
+ON address.addressid = business.addressid
+GROUP BY address.city
+ORDER BY address.city ASC;
+
+
 -------------------------------------------------------------------------------------------------------------------------------------------
 
 -- Q6: From the following tables write a query in SQL to make a list of contacts who are designated as 'Purchasing Manager'. 
@@ -55,12 +81,27 @@
 
 --Answer
 
+SELECT business.businessentityid, person.lastname, person.firstname
+FROM person.businessentitycontact AS business
+JOIN person.person AS person
+	ON person.businessentityid = business.businessentityid
+JOIN person.contacttype AS contact
+	ON contact.contacttypeid = business.contacttypeid
+WHERE contact.name = 'Purchasing Manager'
+ORDER BY lastname ASC, firstname ASC;
+
 -------------------------------------------------------------------------------------------------------------------------------------------
 
 -- Q7: From the following table sales.salesorderdetail  write a query in  SQL to retrieve the total cost of each salesorderID that exceeds 100000. 
 -- Return SalesOrderID, total cost. Round to 2 decimal place and add the dollar sign at the front.
 
 --Answer
+
+SELECT salesorderid, '$' || CAST(ROUND(SUM(unitprice * orderqty), 2) AS VARCHAR(15)) AS total_cost
+FROM sales.salesorderdetail
+GROUP BY salesorderid;
+
+
 
 -------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -69,11 +110,21 @@
 
 --Answer
 
+SELECT lastname, firstname
+FROM person.person
+WHERE lastname LIKE 'R%' AND firstname LIKE '%n'
+ORDER BY firstname ASC, lastname DESC;
+
 -------------------------------------------------------------------------------------------------------------------------------------------
 
 -- Q9: From the following humanresources.department table write a query in  SQL to skip the first 5 rows and return the next 5 rows from the sorted result set.
 
 --Answer
+
+SELECT *
+FROM humanresources.department
+OFFSET 5
+LIMIT 5;
 
 -------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -83,5 +134,12 @@
 -- person.person, person.personphone
 
 --Answer
+
+SELECT person.businessentityid, person.firstname, person.lastname, phone.phonenumber
+FROM person.person AS person
+JOIN person.personphone AS phone
+ON person.businessentityid = phone.businessentityid
+WHERE person.lastname LIKE 'L%'
+ORDER BY person.lastname, person.firstname;
 
 -------------------------------------------------------------------------------------------------------------------------------------------
